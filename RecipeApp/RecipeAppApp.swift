cad2f5fa25b6a56dda30283780357110cc26fa5d
Jonas-serrouya <jonass@balink.net>
2024-11-21 17:11:09 +0200

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Jonas S on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
